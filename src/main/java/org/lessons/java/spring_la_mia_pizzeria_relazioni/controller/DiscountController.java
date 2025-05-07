9eb287241ece75b378bf13aae8d94994d4903cc7
Andre050101 <agiancristiano90@gmail.com>
2025-05-07 17:07:30 +0200

    edit done and working

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/DiscountController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/DiscountController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,4 +31,22 @@ public class DiscountController {
         repo.save(formDiscount);
         return "redirect:/pizzas";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("discount", repo.findById(id).get());
+        return "discounts/edit";
+
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("discount") Discount formDiscount, BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "discounts/edit";
+        }
+        repo.save(formDiscount);
+        return "redirect:/pizzas/" + formDiscount.getPizza().getId();
+    }
+
 }
