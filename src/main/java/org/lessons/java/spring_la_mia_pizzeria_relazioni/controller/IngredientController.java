fa8ed1c03d02db036ae48d1522a59a5d07dd9434
Andre050101 <agiancristiano90@gmail.com>
2025-05-08 12:58:23 +0200

    edit and update ingredient done

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/IngredientController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controller/IngredientController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,6 +39,22 @@ public class IngredientController {
         if (bindingResult.hasErrors()) {
             return "ingredients/create";
         }
+        ingredientRepo.save(formIngredient);
+        return "redirect:/ingredients";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("ingredient", ingredientRepo.findById(id).get());
+        return "ingredients/edit";
+    }
+
+    @PostMapping("edit/{id}")
+    public String update(@Valid @ModelAttribute("ingredient") Ingredient formIngredient, BindingResult binding) {
+        if (binding.hasErrors()) {
+            return "ingredients/edit";
+        }
+
         ingredientRepo.save(formIngredient);
         return "redirect:/ingredients";
     }
